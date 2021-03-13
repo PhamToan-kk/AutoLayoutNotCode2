36508aa968baf4e7fb3ac0d727ba37f3ebe8cbd3
PhamVanToan <pham.van.toan-c@sun-asterisk.com>
2021-03-13 15:14:44 +0700

    Initial Commit

---
+++ AutoLayoutNotCode2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutNotCode2
+//
+//  Created by Pham Toan on 13/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutoLayoutNotCode2Tests/AutoLayoutNotCode2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  AutoLayoutNotCode2Tests.swift
+//  AutoLayoutNotCode2Tests
+//
+//  Created by Pham Toan on 13/03/2021.
+//
+
+import XCTest
+@testable import AutoLayoutNotCode2
+
+class AutoLayoutNotCode2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
